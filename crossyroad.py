5fc352d882a014182489f24d6230e5d063ad1bf2
Neoz06-enzo <enzocalais@icloud.com>
2024-11-18 15:13:53 +0100

    Update crossyroad.py

--- crossyroad.py
+++ crossyroad.py
@@ -1,19 +1,1 @@
-import pygame
-pygame.init()
 
-
-#creer la fenetre du jeu 
-pygame.display.set_mode((800, 600))
-pygame.display.set_caption("CrossyRoad")
-
-
-#boucle du jeu
-
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            
-pygame.quit()
